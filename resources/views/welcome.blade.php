8d1cd987cfc3d6ad2c578c33850f0d5006df28df
“m-tashima” <tashikazu1990@gmail.com>
2024-08-13 22:40:38 +0900

    掲示板雛形作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,30 @@
     <title>掲示板アプリß</title>
 </head>
 <body>
+    <header>
+        <h1 class="title">掲示板</h1>
+        <hr>
+    </header>
+
+    {{-- スレッドエリア --}}
+    <div class="threadWrapper">
+        <div class="childWrapper">
+            <div class="threadTitle">
+                <span>【タイトル】</span>
+                <h1>掲示板アプリ作ってみました。</h1>
+            </div>
+            <section>
+                <div class="wrapper">
+                    <div class="nameArea">
+                        <span>名前：</span>
+                        <p class="username">masa</p>
+                        <time>:2024/8/13 22:40</time>
+                    </div>
+                    <p class="comment">手書きのコメントです。</p>
+                </div>
+            </section>
+        </div>
+    </div>
 
 </body>
 </html>
