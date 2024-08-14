ff668be4e454277dacad78f7624e181adcd7fc59
“m-tashima” <tashikazu1990@gmail.com>
2024-08-14 23:04:51 +0900

    テーブルからコメント取得

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,15 @@ if (isset($_POST["submitButton"])) {
     $body = $_POST["body"];
     var_dump($body);
 }
+
+$comment_array = array();
+// コメントデータをテーブルから取得してくる
+$sql = "SELECT * FROM comment";
+$statement = $pdo -> prepare($sql);
+$statement -> execute();
+$comment_array = $statement;
+var_dump($comment_array -> fetchAll());
+
 ?>
 
 <!DOCTYPE html>
